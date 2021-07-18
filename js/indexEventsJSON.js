474b64ddcee4877f37c7f0672ce5e937d5b5e471
Dariusz Okonski <darek200180@gmail.com>
2021-07-18 20:29:02 +0100

    add files indexEvents.js and indexEventsJSON.js

---
+++ js/indexEventsJSON.js
@@ -0,0 +1,307 @@
+export default [
+    {
+        "h3": "16.11.2019 TARGI ŚLUBNE",
+        "p": "Ofertę przygotowano, voucher czekał na właściciela. Tak spędziliśmy czas na targach ślubnych w Scena Kulinarna Art Restaurant w Bytomiu",
+        "images": [
+            {
+                "dataLightbox": "event-21",
+                "href": "img/events/event21-16-11-2019/01.jpg",
+                "src": "img/events/event21-16-11-2019/01-small.jpg"
+            },
+            {
+                "dataLightbox": "event-21",
+                "href": "img/events/event21-16-11-2019/02.jpg",
+                "src": "img/events/event21-16-11-2019/02-small.jpg"
+            },
+            {
+                "dataLightbox": "event-21",
+                "href": "img/events/event21-16-11-2019/03.jpg",
+                "src": "img/events/event21-16-11-2019/03-small.jpg"
+            },
+            {
+                "dataLightbox": "event-21",
+                "href": "img/events/event21-16-11-2019/04.jpg",
+                "src": "img/events/event21-16-11-2019/04-small.jpg"
+            },
+            {
+                "dataLightbox": "event-21",
+                "href": "img/events/event21-16-11-2019/05.jpg",
+                "src": "img/events/event21-16-11-2019/05-small.jpg"
+            },
+            {
+                "dataLightbox": "event-21",
+                "href": "img/events/event21-16-11-2019/06.jpg",
+                "src": "img/events/event21-16-11-2019/06-small.jpg"
+            },
+            {
+                "dataLightbox": "event-21",
+                "href": "img/events/event21-16-11-2019/07.jpg",
+                "src": "img/events/event21-16-11-2019/07-small.jpg"
+            }
+        ]
+    },
+    {
+        "h3": "19.10.2019 ŚLUB",
+        "p": "I to już ostatnie ZAPLANOWANE wesele w 2019 roku. Te NIEPLANOWANE także realizujemy. A na 2020 rok zapraszamy do kontaktu",
+        "images": [
+            {
+                "dataLightbox": "event-20",
+                "href": "img/events/event20-19-10-2019/01.jpg",
+                "src": "img/events/event20-19-10-2019/01-small.jpg"
+            },
+            {
+                "dataLightbox": "event-20",
+                "href": "img/events/event20-19-10-2019/02.jpg",
+                "src": "img/events/event20-19-10-2019/02-small.jpg"
+            },
+            {
+                "dataLightbox": "event-20",
+                "href": "img/events/event20-19-10-2019/03.jpg",
+                "src": "img/events/event20-19-10-2019/03-small.jpg"
+            },
+            {
+                "dataLightbox": "event-20",
+                "href": "img/events/event20-19-10-2019/04.jpg",
+                "src": "img/events/event20-19-10-2019/04-small.jpg"
+            },
+            {
+                "dataLightbox": "event-20",
+                "href": "img/events/event20-19-10-2019/05.jpg",
+                "src": "img/events/event20-19-10-2019/05-small.jpg"
+            },            
+        ]
+    },  
+    {
+        "h3": "12.10.2019 ŚLUB",
+        "p": "Wszystko było idealnie dopasowane. Sama przyjemność uczestniczyć w takiej ceremonii zaślubin. Młodej Parze dziękujemy za wybór naszego auta do ślubu i oczywiście polecamy się na przyszlość",
+        "images": [
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/01.jpg",
+                "src": "img/events/event19-12-10-2019/01-small.jpg"
+            }, 
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/02.jpg",
+                "src": "img/events/event19-12-10-2019/02-small.jpg"
+            }, 
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/03.jpg",
+                "src": "img/events/event19-12-10-2019/03-small.jpg"
+            }, 
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/04.jpg",
+                "src": "img/events/event19-12-10-2019/04-small.jpg"
+            }, 
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/05.jpg",
+                "src": "img/events/event19-12-10-2019/05-small.jpg"
+            }, 
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/06.jpg",
+                "src": "img/events/event19-12-10-2019/06-small.jpg"
+            }, 
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/07.jpg",
+                "src": "img/events/event19-12-10-2019/07-small.jpg"
+            }, 
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/08.jpg",
+                "src": "img/events/event19-12-10-2019/08-small.jpg"
+            }, 
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/09.jpg",
+                "src": "img/events/event19-12-10-2019/09-small.jpg"
+            }, 
+            {
+                "dataLightbox": "event-19",
+                "href": "img/events/event19-12-10-2019/10.jpg",
+                "src": "img/events/event19-12-10-2019/10-small.jpg"
+            },            
+        ]
+    }, 
+    {
+        "h3": "26.09.2019 ŚLUB",
+        "p": "26.09.2019r czwartek-ślub, 27.09 2019r piątek-poprawiny, 28-29.09.2019r weekend-odpoczynek po lubie. I taki termin w środku tygodnia na ślub nam się podoba. Dziękujemy Młodej Parze za współpracę i życzymy dalszych trafionych terminów w życiu",
+        "images": [
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/01.jpg",
+                "src": "img/events/event18-26-09-2019/01-small.jpg"
+            },
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/02.jpg",
+                "src": "img/events/event18-26-09-2019/02-small.jpg"
+            },
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/03.jpg",
+                "src": "img/events/event18-26-09-2019/03-small.jpg"
+            },
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/04.jpg",
+                "src": "img/events/event18-26-09-2019/04-small.jpg"
+            },
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/05.jpg",
+                "src": "img/events/event18-26-09-2019/05-small.jpg"
+            },
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/06.jpg",
+                "src": "img/events/event18-26-09-2019/06-small.jpg"
+            },
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/07.jpg",
+                "src": "img/events/event18-26-09-2019/07-small.jpg"
+            },
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/08.jpg",
+                "src": "img/events/event18-26-09-2019/08-small.jpg"
+            },
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/09.jpg",
+                "src": "img/events/event18-26-09-2019/09-small.jpg"
+            },
+            {
+                "dataLightbox": "event-18",
+                "href": "img/events/event18-26-09-2019/10.jpg",
+                "src": "img/events/event18-26-09-2019/10-small.jpg"
+            }        
+        ]
+    }, 
+    {
+        "h3": "14.09.2019 ŚLUB",
+        "p": "Kolejna piękna uroczystość za nami. Dziękujemy Młodej Parze za wybór naszego Fiata do ślubu.",
+        "images": [
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/01.jpg",
+                "src": "img/events/event17-14-09-2019/01-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/02.jpg",
+                "src": "img/events/event17-14-09-2019/02-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/03.jpg",
+                "src": "img/events/event17-14-09-2019/03-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/04.jpg",
+                "src": "img/events/event17-14-09-2019/04-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/05.jpg",
+                "src": "img/events/event17-14-09-2019/05-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/06.jpg",
+                "src": "img/events/event17-14-09-2019/06-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/07.jpg",
+                "src": "img/events/event17-14-09-2019/07-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/08.jpg",
+                "src": "img/events/event17-14-09-2019/08-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/09.jpg",
+                "src": "img/events/event17-14-09-2019/09-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/10.jpg",
+                "src": "img/events/event17-14-09-2019/10-small.jpg"
+            },
+            {
+                "dataLightbox": "event-17",
+                "href": "img/events/event17-14-09-2019/11.jpg",
+                "src": "img/events/event17-14-09-2019/11-small.jpg"
+            }            
+        ]
+    }, 
+    {
+        "h3": "21.08.2019. ŚLUB",
+        "p": "W uroczystym dniu zaślubin życzymy Młodej Parze, by im życie dało wszystko, co może dać w darze. Dziękujemy za współpracę i polecamy się na przyszlość.",
+        "images": [
+            {
+                "dataLightbox": "event-16",
+                "href": "img/events/event16-21-08-2019/01.jpg",
+                "src": "img/events/event16-21-08-2019/01-small.jpg"
+            },
+            {
+                "dataLightbox": "event-16",
+                "href": "img/events/event16-21-08-2019/02.jpg",
+                "src": "img/events/event16-21-08-2019/02-small.jpg"
+            },
+            {
+                "dataLightbox": "event-16",
+                "href": "img/events/event16-21-08-2019/03.jpg",
+                "src": "img/events/event16-21-08-2019/03-small.jpg"
+            },
+            {
+                "dataLightbox": "event-16",
+                "href": "img/events/event16-21-08-2019/04.jpg",
+                "src": "img/events/event16-21-08-2019/04-small.jpg"
+            },
+            {
+                "dataLightbox": "event-16",
+                "href": "img/events/event16-21-08-2019/05.jpg",
+                "src": "img/events/event16-21-08-2019/05-small.jpg"
+            },
+            {
+                "dataLightbox": "event-16",
+                "href": "img/events/event16-21-08-2019/06.jpg",
+                "src": "img/events/event16-21-08-2019/06-small.jpg"
+            },
+            {
+                "dataLightbox": "event-16",
+                "href": "img/events/event16-21-08-2019/07.jpg",
+                "src": "img/events/event16-21-08-2019/07-small.jpg"
+            },
+            {
+                "dataLightbox": "event-16",
+                "href": "img/events/event16-21-08-2019/08.jpg",
+                "src": "img/events/event16-21-08-2019/08-small.jpg"
+            }            
+        ]
+    } 
+]
+
+// [
+//     {
+//         "h3": "",
+//         "p": "",
+//         "images": [
+//             {
+//                 "dataLightbox": "",
+//                 "href": "",
+//                 "src": ""
+//             },            
+//         ]
+//     }, 
+// ]
