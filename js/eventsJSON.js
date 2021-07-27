e3088fceeb1d784e81fb98dbdfa6695291924097
Dariusz Okonski <darek200180@gmail.com>
2021-07-27 13:04:10 +0100

    changes at events

--- js/eventsJSON.js
+++ js/eventsJSON.js
@@ -1,161 +1,151 @@
 export default [
     {
-        "h3": "06.2021 ŚLUB",
-        "p": "Z wielkim utęsknieniem......Po długiej przerwie......Udało się zrealizować pierwszy ślub....A teraz czekamy na kolejne",
+        "h3": "09.2020 ŚLUB",
+        "p": "A u nas nie zawsze auto do ślubu prowadzi Pan Młody. ym razem Pani Młoda przejęła stery. I dobrze...... Bo my łamiemy zasady",
         "images": [
             {
-                "dataLightbox": "event-26",
-                "href": "img/events2021/archive/slub3-06-21/1.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/1.jpg"
+                "dataLightbox": "event-30",
+                "href": "img/events2021/slub-09-21/1.jpg",
+                "src": "img/events2021/slub-09-21/small/1.jpg"
             },
             {
-                "dataLightbox": "event-26",
-                "href": "img/events2021/archive/slub3-06-21/2.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/2.jpg"
+                "dataLightbox": "event-30",
+                "href": "img/events2021/slub-09-21/2.jpg",
+                "src": "img/events2021/slub-09-21/small/2.jpg"
             },
             {
-                "dataLightbox": "event-26",
-                "href": "img/events2021/archive/slub3-06-21/3.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/3.jpg"
+                "dataLightbox": "event-30",
+                "href": "img/events2021/slub-09-21/3.jpg",
+                "src": "img/events2021/slub-09-21/small/3.jpg"
             },
             {
-                "dataLightbox": "event-26",
-                "href": "img/events2021/archive/slub3-06-21/4.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/4.jpg"
+                "dataLightbox": "event-30",
+                "href": "img/events2021/slub-09-21/4.jpg",
+                "src": "img/events2021/slub-09-21/small/4.jpg"
             },
             {
-                "dataLightbox": "event-26",
-                "href": "img/events2021/archive/slub3-06-21/5.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/5.jpg"
-            },
-            {
-                "dataLightbox": "event-26",
-                "href": "img/events2021/archive/slub3-06-21/6.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/6.jpg"
-            },
-            {
-                "dataLightbox": "event-26",
-                "href": "img/events2021/archive/slub3-06-21/7.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/7.jpg"
-            },
-            {
-                "dataLightbox": "event-26",
-                "href": "img/events2021/archive/slub3-06-21/8.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/8.jpg"
-            },            
-        ]
-    },
-    {
-        "h3": "06.2021 ŚLUB",
-        "p": "Długo czekaliśmy na ten dzień...Późno ale już oficjalnie i głośno otwieramy SEZON NA WYNAJEM AUT DO ŚLUBU",
-        "images": [
-            {
-                "dataLightbox": "event-25",
-                "href": "img/events2021/archive/slub2-06-21/1.jpg",
-                "src": "img/events2021/archive/slub2-06-21/small/1.jpg"
-            },
-            {
-                "dataLightbox": "event-25",
-                "href": "img/events2021/archive/slub2-06-21/2.jpg",
-                "src": "img/events2021/archive/slub2-06-21/small/2.jpg"
-            },
-            {
-                "dataLightbox": "event-25",
-                "href": "img/events2021/archive/slub2-06-21/3.jpg",
-                "src": "img/events2021/archive/slub2-06-21/small/3.jpg"
-            },
-            {
-                "dataLightbox": "event-25",
-                "href": "img/events2021/archive/slub2-06-21/4.jpg",
-                "src": "img/events2021/archive/slub2-06-21/small/4.jpg"
-            },
-            {
-                "dataLightbox": "event-25",
-                "href": "img/events2021/archive/slub2-06-21/5.jpg",
-                "src": "img/events2021/archive/slub2-06-21/small/5.jpg"
+                "dataLightbox": "event-30",
+                "href": "img/events2021/slub-09-21/5.jpg",
+                "src": "img/events2021/slub-09-21/small/5.jpg"
             }            
         ]
     },
     {
-        "h3": "06.2021 ŚLUB",
-        "p": "Z wielkim utęsknieniem......Po długiej przerwie......Udało się zrealizować pierwszy ślub....A teraz czekamy na kolejne",
+        "h3": "08.2020 ŚLUB",
+        "p": "W tym wyjątkowym dniu nie było znaczenia, że padał deszcz. Najważniejsze, że wszystko udało się po myśli Mlodej Pary",
         "images": [
             {
-                "dataLightbox": "event-24",
-                "href": "img/events2021/archive/slub3-06-21/1.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/1.jpg"
+                "dataLightbox": "event-29",
+                "href": "img/events2021/slub3-08-21/1.jpg",
+                "src": "img/events2021/slub3-08-21/small/1.jpg"
             },
             {
-                "dataLightbox": "event-24",
-                "href": "img/events2021/archive/slub3-06-21/2.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/2.jpg"
+                "dataLightbox": "event-29",
+                "href": "img/events2021/slub3-08-21/2.jpg",
+                "src": "img/events2021/slub3-08-21/small/2.jpg"
             },
             {
-                "dataLightbox": "event-24",
-                "href": "img/events2021/archive/slub3-06-21/3.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/3.jpg"
+                "dataLightbox": "event-29",
+                "href": "img/events2021/slub3-08-21/3.jpg",
+                "src": "img/events2021/slub3-08-21/small/3.jpg"
             },
             {
-                "dataLightbox": "event-24",
-                "href": "img/events2021/archive/slub3-06-21/4.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/4.jpg"
+                "dataLightbox": "event-29",
+                "href": "img/events2021/slub3-08-21/4.jpg",
+                "src": "img/events2021/slub3-08-21/small/4.jpg"
             },
             {
-                "dataLightbox": "event-24",
-                "href": "img/events2021/archive/slub3-06-21/5.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/5.jpg"
+                "dataLightbox": "event-29",
+                "href": "img/events2021/slub3-08-21/5.jpg",
+                "src": "img/events2021/slub3-08-21/small/5.jpg"
             },
             {
-                "dataLightbox": "event-24",
-                "href": "img/events2021/archive/slub3-06-21/6.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/6.jpg"
+                "dataLightbox": "event-29",
+                "href": "img/events2021/slub3-08-21/6.jpg",
+                "src": "img/events2021/slub3-08-21/small/6.jpg"
             },
             {
-                "dataLightbox": "event-24",
-                "href": "img/events2021/archive/slub3-06-21/7.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/7.jpg"
+                "dataLightbox": "event-29",
+                "href": "img/events2021/slub3-08-21/7.jpg",
+                "src": "img/events2021/slub3-08-21/small/7.jpg"
             },
             {
-                "dataLightbox": "event-24",
-                "href": "img/events2021/archive/slub3-06-21/8.jpg",
-                "src": "img/events2021/archive/slub3-06-21/small/8.jpg"
+                "dataLightbox": "event-29",
+                "href": "img/events2021/slub3-08-21/8.jpg",
+                "src": "img/events2021/slub3-08-21/small/8.jpg"
             },            
         ]
     },
     {
-        "h3": "01.2021 ŚLUB",
-        "p": "I oto tym miłym akcentem otwieramy Sezon Slubny 2020 na Fiata. Pierwsze zlecenie w tym roku już wykonane",
+        "h3": "08.2020 ŚLUB",
+        "p": "Dla odmiany ślub w klimacie sportowym. Zapraszamy do rezerwacji nie tylko Fiata ale i Mustanga",
         "images": [
             {
-                "dataLightbox": "event-23",
-                "href": "img/events2021/archive/slub-01-21/1.jpg",
-                "src": "img/events2021/archive/slub-01-21/small/1.jpg"
+                "dataLightbox": "event-28",
+                "href": "img/events2021/slub2-08-21/1.jpg",
+                "src": "img/events2021/slub2-08-21/small/1.jpg"
             },
             {
-                "dataLightbox": "event-23",
-                "href": "img/events2021/archive/slub-01-21/2.jpg",
-                "src": "img/events2021/archive/slub-01-21/small/2.jpg"
+                "dataLightbox": "event-28",
+                "href": "img/events2021/slub2-08-21/2.jpg",
+                "src": "img/events2021/slub2-08-21/small/2.jpg"
             },
             {
-                "dataLightbox": "event-23",
-                "href": "img/events2021/archive/slub-01-21/3.jpg",
-                "src": "img/events2021/archive/slub-01-21/small/3.jpg"
+                "dataLightbox": "event-28",
+                "href": "img/events2021/slub2-08-21/3.jpg",
+                "src": "img/events2021/slub2-08-21/small/3.jpg"
             },
             {
-                "dataLightbox": "event-23",
-                "href": "img/events2021/archive/slub-01-21/4.jpg",
-                "src": "img/events2021/archive/slub-01-21/small/4.jpg"
+                "dataLightbox": "event-28",
+                "href": "img/events2021/slub2-08-21/4.jpg",
+                "src": "img/events2021/slub2-08-21/small/4.jpg"
             },
             {
-                "dataLightbox": "event-23",
-                "href": "img/events2021/archive/slub-01-21/5.jpg",
-                "src": "img/events2021/archive/slub-01-21/small/5.jpg"
-            },
-            {
-                "dataLightbox": "event-23",
-                "href": "img/events2021/archive/slub-01-21/6.jpg",
-                "src": "img/events2021/archive/slub-01-21/small/6.jpg"
+                "dataLightbox": "event-28",
+                "href": "img/events2021/slub2-08-21/5.jpg",
+                "src": "img/events2021/slub2-08-21/small/5.jpg"
             }            
+        ]
+    }, 
+    {
+        "h3": "08.2020 ŚLUB",
+        "p": "Uciekająca Panna Młoda? Nieeeeeee, to tylko sesja zdjęciowa",
+        "images": [
+            {
+                "dataLightbox": "event-27",
+                "href": "img/events2021/slub-08-21/1.jpg",
+                "src": "img/events2021/slub-08-21/small/1.jpg"
+            },
+            {
+                "dataLightbox": "event-27",
+                "href": "img/events2021/slub-08-21/2.jpg",
+                "src": "img/events2021/slub-08-21/small/2.jpg"
+            },
+            {
+                "dataLightbox": "event-27",
+                "href": "img/events2021/slub-08-21/3.jpg",
+                "src": "img/events2021/slub-08-21/small/3.jpg"
+            },
+            {
+                "dataLightbox": "event-27",
+                "href": "img/events2021/slub-08-21/4.jpg",
+                "src": "img/events2021/slub-08-21/small/4.jpg"
+            },
+            {
+                "dataLightbox": "event-27",
+                "href": "img/events2021/slub-08-21/5.jpg",
+                "src": "img/events2021/slub-08-21/small/5.jpg"
+            },
+            {
+                "dataLightbox": "event-27",
+                "href": "img/events2021/slub-08-21/6.jpg",
+                "src": "img/events2021/slub-08-21/small/6.jpg"
+            },
+            {
+                "dataLightbox": "event-27",
+                "href": "img/events2021/slub-08-21/7.jpg",
+                "src": "img/events2021/slub-08-21/small/7.jpg"
+            },
         ]
     },
     {
--- js/indexEventsJSON.js
+++ js/indexEventsJSON.js
@@ -1,6 +1,165 @@
 export default [
     {
-        "h3": "10.2021 ŚLUB",
+        "h3": "06.2021 ŚLUB",
+        "p": "Z wielkim utęsknieniem......Po długiej przerwie......Udało się zrealizować pierwszy ślub....A teraz czekamy na kolejne",
+        "images": [
+            {
+                "dataLightbox": "event-26",
+                "href": "img/events2021/archive/slub3-06-21/1.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/1.jpg"
+            },
+            {
+                "dataLightbox": "event-26",
+                "href": "img/events2021/archive/slub3-06-21/2.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/2.jpg"
+            },
+            {
+                "dataLightbox": "event-26",
+                "href": "img/events2021/archive/slub3-06-21/3.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/3.jpg"
+            },
+            {
+                "dataLightbox": "event-26",
+                "href": "img/events2021/archive/slub3-06-21/4.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/4.jpg"
+            },
+            {
+                "dataLightbox": "event-26",
+                "href": "img/events2021/archive/slub3-06-21/5.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/5.jpg"
+            },
+            {
+                "dataLightbox": "event-26",
+                "href": "img/events2021/archive/slub3-06-21/6.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/6.jpg"
+            },
+            {
+                "dataLightbox": "event-26",
+                "href": "img/events2021/archive/slub3-06-21/7.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/7.jpg"
+            },
+            {
+                "dataLightbox": "event-26",
+                "href": "img/events2021/archive/slub3-06-21/8.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/8.jpg"
+            },            
+        ]
+    },
+    {
+        "h3": "06.2021 ŚLUB",
+        "p": "Długo czekaliśmy na ten dzień...Późno ale już oficjalnie i głośno otwieramy SEZON NA WYNAJEM AUT DO ŚLUBU",
+        "images": [
+            {
+                "dataLightbox": "event-25",
+                "href": "img/events2021/archive/slub2-06-21/1.jpg",
+                "src": "img/events2021/archive/slub2-06-21/small/1.jpg"
+            },
+            {
+                "dataLightbox": "event-25",
+                "href": "img/events2021/archive/slub2-06-21/2.jpg",
+                "src": "img/events2021/archive/slub2-06-21/small/2.jpg"
+            },
+            {
+                "dataLightbox": "event-25",
+                "href": "img/events2021/archive/slub2-06-21/3.jpg",
+                "src": "img/events2021/archive/slub2-06-21/small/3.jpg"
+            },
+            {
+                "dataLightbox": "event-25",
+                "href": "img/events2021/archive/slub2-06-21/4.jpg",
+                "src": "img/events2021/archive/slub2-06-21/small/4.jpg"
+            },
+            {
+                "dataLightbox": "event-25",
+                "href": "img/events2021/archive/slub2-06-21/5.jpg",
+                "src": "img/events2021/archive/slub2-06-21/small/5.jpg"
+            }            
+        ]
+    },
+    {
+        "h3": "06.2021 ŚLUB",
+        "p": "Z wielkim utęsknieniem......Po długiej przerwie......Udało się zrealizować pierwszy ślub....A teraz czekamy na kolejne",
+        "images": [
+            {
+                "dataLightbox": "event-24",
+                "href": "img/events2021/archive/slub3-06-21/1.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/1.jpg"
+            },
+            {
+                "dataLightbox": "event-24",
+                "href": "img/events2021/archive/slub3-06-21/2.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/2.jpg"
+            },
+            {
+                "dataLightbox": "event-24",
+                "href": "img/events2021/archive/slub3-06-21/3.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/3.jpg"
+            },
+            {
+                "dataLightbox": "event-24",
+                "href": "img/events2021/archive/slub3-06-21/4.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/4.jpg"
+            },
+            {
+                "dataLightbox": "event-24",
+                "href": "img/events2021/archive/slub3-06-21/5.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/5.jpg"
+            },
+            {
+                "dataLightbox": "event-24",
+                "href": "img/events2021/archive/slub3-06-21/6.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/6.jpg"
+            },
+            {
+                "dataLightbox": "event-24",
+                "href": "img/events2021/archive/slub3-06-21/7.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/7.jpg"
+            },
+            {
+                "dataLightbox": "event-24",
+                "href": "img/events2021/archive/slub3-06-21/8.jpg",
+                "src": "img/events2021/archive/slub3-06-21/small/8.jpg"
+            },            
+        ]
+    },
+    {
+        "h3": "01.2021 ŚLUB",
+        "p": "I oto tym miłym akcentem otwieramy Sezon Slubny 2021 na Fiata. Pierwsze zlecenie w tym roku już wykonane",
+        "images": [
+            {
+                "dataLightbox": "event-23",
+                "href": "img/events2021/archive/slub-01-21/1.jpg",
+                "src": "img/events2021/archive/slub-01-21/small/1.jpg"
+            },
+            {
+                "dataLightbox": "event-23",
+                "href": "img/events2021/archive/slub-01-21/2.jpg",
+                "src": "img/events2021/archive/slub-01-21/small/2.jpg"
+            },
+            {
+                "dataLightbox": "event-23",
+                "href": "img/events2021/archive/slub-01-21/3.jpg",
+                "src": "img/events2021/archive/slub-01-21/small/3.jpg"
+            },
+            {
+                "dataLightbox": "event-23",
+                "href": "img/events2021/archive/slub-01-21/4.jpg",
+                "src": "img/events2021/archive/slub-01-21/small/4.jpg"
+            },
+            {
+                "dataLightbox": "event-23",
+                "href": "img/events2021/archive/slub-01-21/5.jpg",
+                "src": "img/events2021/archive/slub-01-21/small/5.jpg"
+            },
+            {
+                "dataLightbox": "event-23",
+                "href": "img/events2021/archive/slub-01-21/6.jpg",
+                "src": "img/events2021/archive/slub-01-21/small/6.jpg"
+            }            
+        ]
+    },
+    {
+        "h3": "10.2020 ŚLUB",
         "p": " tak powoli zbliżamy się do końca sezonu ślubnego w 2020 roku. Były śluby zaplanowane.......Teraz czekamy na ślubu niezaplanowane i nie tylko śluby.",
         "images": [
             {
@@ -36,7 +195,7 @@ export default [
         ]
     },
     {
-        "h3": "10.2021 ŚLUB",
+        "h3": "10.2020 ŚLUB",
         "p": "Nie potrafiłam się zdecydować, które zdjęcie z poniższych wybrać jako jedyne przedstawiające ostatnie w tym roku nasze zlecenie ślubne. Więc wybrałam wszystkie.",
         "images": [
             {
@@ -79,155 +238,6 @@ export default [
                 "href": "img/events2021/slub-10-21/8.jpg",
                 "src": "img/events2021/slub-10-21/small/8.jpg"
             }            
-        ]
-    },
-    {
-        "h3": "09.2021 ŚLUB",
-        "p": "A u nas nie zawsze auto do ślubu prowadzi Pan Młody. ym razem Pani Młoda przejęła stery. I dobrze...... Bo my łamiemy zasady",
-        "images": [
-            {
-                "dataLightbox": "event-30",
-                "href": "img/events2021/slub-09-21/1.jpg",
-                "src": "img/events2021/slub-09-21/small/1.jpg"
-            },
-            {
-                "dataLightbox": "event-30",
-                "href": "img/events2021/slub-09-21/2.jpg",
-                "src": "img/events2021/slub-09-21/small/2.jpg"
-            },
-            {
-                "dataLightbox": "event-30",
-                "href": "img/events2021/slub-09-21/3.jpg",
-                "src": "img/events2021/slub-09-21/small/3.jpg"
-            },
-            {
-                "dataLightbox": "event-30",
-                "href": "img/events2021/slub-09-21/4.jpg",
-                "src": "img/events2021/slub-09-21/small/4.jpg"
-            },
-            {
-                "dataLightbox": "event-30",
-                "href": "img/events2021/slub-09-21/5.jpg",
-                "src": "img/events2021/slub-09-21/small/5.jpg"
-            }            
-        ]
-    },
-    {
-        "h3": "08.2021 ŚLUB",
-        "p": "W tym wyjątkowym dniu nie było znaczenia, że padał deszcz. Najważniejsze, że wszystko udało się po myśli Mlodej Pary",
-        "images": [
-            {
-                "dataLightbox": "event-29",
-                "href": "img/events2021/slub3-08-21/1.jpg",
-                "src": "img/events2021/slub3-08-21/small/1.jpg"
-            },
-            {
-                "dataLightbox": "event-29",
-                "href": "img/events2021/slub3-08-21/2.jpg",
-                "src": "img/events2021/slub3-08-21/small/2.jpg"
-            },
-            {
-                "dataLightbox": "event-29",
-                "href": "img/events2021/slub3-08-21/3.jpg",
-                "src": "img/events2021/slub3-08-21/small/3.jpg"
-            },
-            {
-                "dataLightbox": "event-29",
-                "href": "img/events2021/slub3-08-21/4.jpg",
-                "src": "img/events2021/slub3-08-21/small/4.jpg"
-            },
-            {
-                "dataLightbox": "event-29",
-                "href": "img/events2021/slub3-08-21/5.jpg",
-                "src": "img/events2021/slub3-08-21/small/5.jpg"
-            },
-            {
-                "dataLightbox": "event-29",
-                "href": "img/events2021/slub3-08-21/6.jpg",
-                "src": "img/events2021/slub3-08-21/small/6.jpg"
-            },
-            {
-                "dataLightbox": "event-29",
-                "href": "img/events2021/slub3-08-21/7.jpg",
-                "src": "img/events2021/slub3-08-21/small/7.jpg"
-            },
-            {
-                "dataLightbox": "event-29",
-                "href": "img/events2021/slub3-08-21/8.jpg",
-                "src": "img/events2021/slub3-08-21/small/8.jpg"
-            },            
-        ]
-    },
-    {
-        "h3": "08.2021 ŚLUB",
-        "p": "Dla odmiany ślub w klimacie sportowym. Zapraszamy do rezerwacji nie tylko Fiata ale i Mustanga",
-        "images": [
-            {
-                "dataLightbox": "event-28",
-                "href": "img/events2021/slub2-08-21/1.jpg",
-                "src": "img/events2021/slub2-08-21/small/1.jpg"
-            },
-            {
-                "dataLightbox": "event-28",
-                "href": "img/events2021/slub2-08-21/2.jpg",
-                "src": "img/events2021/slub2-08-21/small/2.jpg"
-            },
-            {
-                "dataLightbox": "event-28",
-                "href": "img/events2021/slub2-08-21/3.jpg",
-                "src": "img/events2021/slub2-08-21/small/3.jpg"
-            },
-            {
-                "dataLightbox": "event-28",
-                "href": "img/events2021/slub2-08-21/4.jpg",
-                "src": "img/events2021/slub2-08-21/small/4.jpg"
-            },
-            {
-                "dataLightbox": "event-28",
-                "href": "img/events2021/slub2-08-21/5.jpg",
-                "src": "img/events2021/slub2-08-21/small/5.jpg"
-            }            
-        ]
-    }, 
-    {
-        "h3": "08.2021 ŚLUB",
-        "p": "Uciekająca Panna Młoda? Nieeeeeee, to tylko sesja zdjęciowa",
-        "images": [
-            {
-                "dataLightbox": "event-27",
-                "href": "img/events2021/slub-08-21/1.jpg",
-                "src": "img/events2021/slub-08-21/small/1.jpg"
-            },
-            {
-                "dataLightbox": "event-27",
-                "href": "img/events2021/slub-08-21/2.jpg",
-                "src": "img/events2021/slub-08-21/small/2.jpg"
-            },
-            {
-                "dataLightbox": "event-27",
-                "href": "img/events2021/slub-08-21/3.jpg",
-                "src": "img/events2021/slub-08-21/small/3.jpg"
-            },
-            {
-                "dataLightbox": "event-27",
-                "href": "img/events2021/slub-08-21/4.jpg",
-                "src": "img/events2021/slub-08-21/small/4.jpg"
-            },
-            {
-                "dataLightbox": "event-27",
-                "href": "img/events2021/slub-08-21/5.jpg",
-                "src": "img/events2021/slub-08-21/small/5.jpg"
-            },
-            {
-                "dataLightbox": "event-27",
-                "href": "img/events2021/slub-08-21/6.jpg",
-                "src": "img/events2021/slub-08-21/small/6.jpg"
-            },
-            {
-                "dataLightbox": "event-27",
-                "href": "img/events2021/slub-08-21/7.jpg",
-                "src": "img/events2021/slub-08-21/small/7.jpg"
-            },
         ]
     }
 ]
